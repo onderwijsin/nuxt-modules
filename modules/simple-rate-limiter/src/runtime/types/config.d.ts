0fa4e51ea25970b98599cd0c9919464c599d6435
Remi Huigen <remihuigen@proton.me>
2026-08-10 09:16:19 +0200

    fix: simple rate limiter tsconfig

--- modules/simple-rate-limiter/src/runtime/types/config.d.ts
+++ modules/simple-rate-limiter/src/runtime/types/config.d.ts
@@ -1,16 +1,14 @@
-interface SimpleRateLimiterRuntimeConfig {
-  global: {
-    enabled: boolean;
-    pruning: {
-      enabled: boolean;
-      staleAfter: number;
-    };
-  };
-}
-
 declare module "nuxt/schema" {
   interface RuntimeConfig {
-    simpleRateLimiter: SimpleRateLimiterRuntimeConfig;
+    simpleRateLimiter: {
+      global: {
+        enabled: boolean;
+        pruning: {
+          enabled: boolean;
+          staleAfter: number;
+        };
+      };
+    };
   }
 }
 
